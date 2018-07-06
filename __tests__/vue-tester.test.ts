1c3cd487c3b6ed9b6f069ec09a58de4b3041a0f7
Jonathan Johnson <me@jondavidjohn.com>
2018-07-05 19:47:51 -0500

    complete specs for vue interactions

--- __tests__/vue-tester.test.ts
+++ __tests__/vue-tester.test.ts
@@ -1,21 +1,64 @@
+import Vue from 'vue'
 import VueTester from '../lib/vue-tester'
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 
-import TestComponent from './test-component'
+const TestComponent = Vue.extend({
+  template: `
+    <div>
+      <p>{{ message }}</p>
+      <input name="message" type="text" v-model="inputValue"/>
+      <button @click="buttonClick">Click Me!</button>
+    </div>
+  `,
+
+  data() {
+    return {
+      inputValue: '',
+      message: 'Hello World'
+    }
+  },
+
+  methods: {
+    buttonClick() {
+      this.message = this.inputValue
+    }
+  }
+})
 
 describe('VueTester', () => {
   describe('#fillIn', () => {
     it('updates the named input value', () => {
+      const wrapper = mount(TestComponent)
+      const tester = new VueTester(wrapper)
+
+      tester.fillIn('message', 'Hello Universe!')
+      expect(wrapper.vm.inputValue).toBe('Hello Universe!')
     })
   })
 
   describe('#click', () => {
     it('it triggers a click on the selected element', () => {
+      const buttonClick = jest.fn()
+      const wrapper = mount(TestComponent, {
+        methods: { buttonClick }
+      })
+      const tester = new VueTester(wrapper)
+
+      tester.click('button')
+      expect(buttonClick).toHaveBeenCalled()
     })
   })
 
   describe('#integration', () => {
     it('produces expected diffs', () => {
+      const wrapper = mount(TestComponent)
+      const tester = new VueTester(wrapper)
+
+      expect(tester.current()).toMatchSnapshot()
+
+      tester.fillIn('message', 'Hello Universe!')
+      tester.click('button')
+      expect(tester.next()).toMatchSnapshot()
     })
   })
 })
--- lib/serializer.ts
+++ lib/serializer.ts
@@ -1,5 +1,5 @@
-import * as vue from 'jest-serializer-vue'
-import * as diff from 'snapshot-diff-serializer'
+import vue from 'jest-serializer-vue'
+import diff from 'snapshot-diff-serializer'
 
 function test(value: string) : boolean {
   return vue.test(value) || diff.test(value)
